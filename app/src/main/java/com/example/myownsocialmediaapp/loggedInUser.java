14cd976b6326096cd81fff621b0c6ba7cfd8942f
ANKIT RAJ <ankitraj1602@gmail.com>
2020-03-11 20:08:55 +0530

    Users tab has been implemented, though the highlighter in tab constantly appears when the log in happens. I will try to undo that

--- app/src/main/java/com/example/myownsocialmediaapp/loggedInUser.java
+++ app/src/main/java/com/example/myownsocialmediaapp/loggedInUser.java
@@ -1,44 +1,70 @@
 package com.example.myownsocialmediaapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.android.material.tabs.TabLayout;
 import com.parse.LogOutCallback;
 import com.parse.ParseException;
 import com.parse.ParseUser;
 
-public class loggedInUser extends AppCompatActivity implements View.OnClickListener {
-private Button logOutLoggedIn;
+public class loggedInUser extends AppCompatActivity {
+
+    private ViewPager mViewPager;
+    private TabLayout mTabLayout;
+    private TabAdapter mTabAdapter;
+    private Toolbar mToolbar;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logged_in_user);
-        logOutLoggedIn = findViewById(R.id.logOutLoggedInBtn);
 
-        logOutLoggedIn.setOnClickListener(this);
+        setTitle("MY SOCIAL MEDIA APP");
+
+        mViewPager = findViewById(R.id.myViewPager);
+        mTabLayout = findViewById(R.id.myTabLayout);
+        mToolbar = findViewById(R.id.myToolbar);
+        setSupportActionBar(mToolbar);
+
+        mTabAdapter = new TabAdapter(getSupportFragmentManager());
+        mViewPager.setAdapter(mTabAdapter);
+
+        mTabLayout.setupWithViewPager(mViewPager,true);
+
+
+
 
 
     }
 
-    @Override
-    public void onClick(View v) {
-        ParseUser.getCurrentUser().logOutInBackground(new LogOutCallback() {
-            @Override
-            public void done(ParseException e) {
-                if(e==null){
+//    @Override
+//    public void onClick(View v) {
+//        ParseUser.getCurrentUser().logOutInBackground(new LogOutCallback() {
+//            @Override
+//            public void done(ParseException e) {
+//                if(e==null){
+//
+//                    finish();
+//                }
+//                else{
+//                    Toast.makeText(loggedInUser.this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+//                }
+//            }
+//        });
+//    }
 
-                    finish();
-                }
-                else{
-                    Toast.makeText(loggedInUser.this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
-    }
+
+
+
+
+
+
 }
---
+++ app/src/main/java/com/example/myownsocialmediaapp/profileTab.java
@@ -0,0 +1,109 @@
+package com.example.myownsocialmediaapp;
+
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class profileTab extends Fragment implements View.OnClickListener {
+
+    private EditText profileHobby,profileName,profileAge,
+            profileProfession,profileSport;
+
+    private Button btnProfileUpdateInfo;
+
+    public profileTab() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_profile_tab, container, false);
+
+        profileName = view.findViewById(R.id.edtProfileName);
+        profileAge = view.findViewById(R.id.edtProfileAge);
+        profileHobby = view.findViewById(R.id.edtProfileHobby);
+        profileSport = view.findViewById(R.id.edtProfileSport);
+        profileProfession = view.findViewById(R.id.edtProfileProfession);
+
+        btnProfileUpdateInfo = view.findViewById(R.id.updateProfileInfoBtn);
+        btnProfileUpdateInfo.setOnClickListener(this);
+
+        ParseUser user = ParseUser.getCurrentUser();
+
+        if(user.get("ProfileName")==null){
+            profileName.setText("");}
+        else{
+        profileName.setText(user.get("ProfileName")+"");}
+
+        if(user.get("ProfileProfession")==null)
+        {profileProfession.setText("");}
+        else{profileProfession.setText(user.get("ProfileProfession")+"");}
+
+        if(user.get("ProfileSport")==null)
+        {profileSport.setText("");}
+        else{profileSport.setText(user.get("ProfileSport")+"");}
+
+        if(user.get("ProfileHobby")==null){
+            profileHobby.setText("");
+        }
+       else{ profileHobby.setText(user.get("ProfileHobby")+"");}
+
+       if(user.get("ProfileAge")==null)
+       {profileAge.setText("");}
+       else{profileAge.setText(user.get("ProfileAge")+"");}
+
+
+
+           return view;
+    }
+
+    @Override
+    public void onClick(View v) {
+        ParseUser user = ParseUser.getCurrentUser();
+        user.put("ProfileName",profileName.getText().toString());
+        user.put("ProfileAge",profileAge.getText().toString());
+        user.put("ProfileProfession",profileProfession.getText().toString());
+        user.put("ProfilleSport",profileSport.getText().toString());
+        user.put("ProfileHobby",profileHobby.getText().toString());
+
+        final ProgressDialog progressDialog = new ProgressDialog(getContext());
+        progressDialog.setMessage("SAVING YOUR PROFILE INFORMATION");
+        progressDialog.show();
+        user.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if(e==null){
+                    Toast.makeText(getContext(), "Info Saved", Toast.LENGTH_SHORT).show();
+
+
+                }
+                else{
+                    Toast.makeText(getContext(), ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+                progressDialog.dismiss();
+            }
+
+        });
+
+    }
+}
